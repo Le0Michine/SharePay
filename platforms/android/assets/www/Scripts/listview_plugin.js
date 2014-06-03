28912f4915f8bda8d754bdc02df1bd56549c0fd5
Leo <leomichine@gmail.com>
2014-06-04 02:02:53 +0400

    added bill list, debt list

--- platforms/android/assets/www/Scripts/listview_plugin.js
+++ platforms/android/assets/www/Scripts/listview_plugin.js
@@ -4,9 +4,9 @@ plugin_listview={
 		this.selectedItems=new Object();
 	},
 	
-	listItemSelect: function(itemId, selectedItems){
+	listItemMultiSelect: function(itemId){
 		var listItem=$("#"+itemId);
-		if(this.selectedItems[itemId]){
+		if(itemId in this.selectedItems){
 			listItem.removeClass( "ui-btn-active" );
 			delete(this.selectedItems[itemId]);
 		}
@@ -14,6 +14,42 @@ plugin_listview={
 			listItem.addClass( "ui-btn-active" );
 			this.selectedItems[itemId]=true;
 		}
+	},
+	
+	listItemSingleSelect: function(listItem) {
+		var tmp=true;
+		if(listItem in this.selectedItems){
+			tmp=false;
+		}
+		for(key in this.selectedItems) {
+			$(key).removeClass( "ui-btn-active" );
+			delete(this.selectedItems[key]);
+		}
+		if(tmp) {
+			$(listItem).addClass( "ui-btn-active" );
+			this.selectedItems[listItem]=true;
+		}
+	},
+	
+	fill_list: function(listId) {
+		var item ="<li id=\"123\"><a href=\"#\" ><h3>Avery Walker</h3><p class=\"topic\"><strong>Re: Dinner Tonight</strong></p><p>Sure, let's plan on meeting at Highland Kitchen at 8:00 tonight. Can't wait!</p><p class=\"ui-li-aside\"><strong>4:48</strong>PM</p></a><a href=\"#\" cass=\"delete\" onclick=\"plugin_listview.delete_item(this.parentElement)\">Delete</a></li>";
+		var list=$("#"+listId);
+		list.append(item);
+		list.listview("refresh");
+	},
+	
+	delete_item: function(listItem,popupId) {
+		$(listItem).children(".ui-btn").addClass("ui-btn-active");
+		$("#"+popupId+" .topic").remove();
+        $(listItem).find(".topic").clone().insertAfter("#question");
+        $("#"+popupId).popup("open");
+		$("#"+popupId+" #yes").on("click", function() {
+			$(listItem).remove();
+			$(listItem.parentElement).listview( 'refresh' );
+		});
+		$("#"+popupId+" #cancel").on( "click", function() {
+            $(listItem).children(".ui-btn").removeClass("ui-btn-active");
+            $("#"+popupId+" #yes").off();
+        });
 	}
-
 }
